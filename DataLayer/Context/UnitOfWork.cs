67f9c570c1e453357f5d607b9db06e93c109e4ee
hashemkhajepour <hashemkhajepour2021@gmail.com>
2020-10-26 10:25:32 +0330

    LayoutDefualt

---
+++ DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class UnitOfWork : IDisposable
+    {
+        MyAmazon_Context db = new MyAmazon_Context();
+
+        private Generic_Repo_Amazon<Users> _userRepository;
+        public Generic_Repo_Amazon<Users> UserRepository
+        {
+            get
+            {
+                if(_userRepository == null)
+                {
+                    _userRepository = new Generic_Repo_Amazon<Users>(db);
+                }
+                return _userRepository;
+            }
+        }
+        
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
